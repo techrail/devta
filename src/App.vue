b71a643a282b7594939e48c409e1f42c06616409
Vaibhav <vaibhavkaushal123@gmail.com>
2023-07-27 21:57:02 +0530

    made a little change

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,8 @@
   <div class="d-flex flex-col justify-content-center min-vh-100 align-items-center ">
     <h1 class="text-muted">
       Hello world!
+      Hi I am Vaibhav.
+      <button @click="count++">{{ count }}</button>
     </h1>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -4,4 +4,13 @@ import App from "./App.vue";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 
+const app = createApp({
+  data() {
+    return {
+      count: 0
+    }
+  }
+})
+
 createApp(App).mount("#app");
+
