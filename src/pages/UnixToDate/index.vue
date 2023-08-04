7ce1ba6c2495f3faff9e6db49a79bb39287eaf31
Harikrishna-28 <harikrishnac2002@gmail.com>
2023-08-04 19:23:59 +0530

    add copy to unix-date

--- src/pages/UnixToDate/index.vue
+++ src/pages/UnixToDate/index.vue
@@ -3,6 +3,7 @@ import { ref } from 'vue';
 import { formatDates, labelFormatter } from '../../components/utils/UnixDateTimeFunctions';
 import DateTimeConstants from '../../components/DateTimeConstants/DateTimeConstants.vue';
 import { copyToClipboard } from '../../components/utils/UnixDateTimeFunctions';
+import CopyContainer from '../../components/CopyContainer/CopyContainer.vue';
 
 const unix = ref()
 const data = ref()
@@ -38,8 +39,9 @@ const handleClick = (value) => {
         <div class="block card block2 overflow-auto">
             <div v-if="unix" class="mt-3">
                 <div v-for="(value, key) in data" class="p-1 d-flex flex-column gap-2" :key="key">
-                    <div role="button" @click="handleClick(value)" class="card p-1 flex flex-row">
-                        <strong> {{ labelFormatter(key) }} </strong> : {{ value }}
+                    <div role="button" @click="handleClick(value)" class="p-1 flex flex-row">
+                        <CopyContainer :title="key" :value="value" />
+                        <!-- <strong> {{ labelFormatter(key) }} </strong> : {{ value }} -->
                     </div>
                 </div>
             </div>
