311f4ae66433ca487d666d73668cc5130963c6e4
Harikrishna-28 <harikrishnac2002@gmail.com>
2023-08-07 13:39:53 +0530

    add file upload for markdown

--- src/pages/MarkdownPreview/index.vue
+++ src/pages/MarkdownPreview/index.vue
@@ -4,9 +4,31 @@ import { copyToClipboard } from '../../components/utils/UnixDateTimeFunctions';
 import { marked } from 'marked';
 
 const markdown = ref()
+const file = ref()
+
 const handleClick = (value) => {
     copyToClipboard(value)
 }
+
+const handleFileChange = async (event) => {
+    const file = event.target.files[0];
+    if (file) {
+        if (file.name.endsWith(".md")) {
+            const reader = new FileReader();
+            reader.onload = async () => {
+                try {
+                    markdown.value = reader.result;
+                } catch (error) {
+                    console.error("Error parsing Markdown:", error);
+                }
+            };
+            reader.readAsText(file);
+        } else {
+            console.log("Please select a .md file under 1 MB.");
+        }
+    }
+}
+
 </script>
 
 <template>
@@ -22,9 +44,18 @@ const handleClick = (value) => {
                     <textarea v-model="markdown" autofocus type="text" class="form-control" rows="15"
                         placeholder="Enter markdown">
                     </textarea>
-                    <button class="btn btn-primary mt-2" type="button" @click="handleClick(markdown)">
+
+                    <!-- file input that only accpets markdown files lesser than 1mb -->
+                    <input type="file" @change="handleFileChange" size="1048576" class="form-control mt-1" accept=".md"
+                        name="Upload markdown file" id="check">
+
+                    <button class="btn btn-primary mt-1" type="button" @click="handleClick(markdown)">
                         Copy markdown
                     </button>
+
+                </div>
+                <div class="d-flex flex-row">
+
                 </div>
 
             </div>
