a392325f532ddb365830b37220bc0303b9ac0b3c
Harikrishna-28 <harikrishnac2002@gmail.com>
2023-08-12 17:41:41 +0530

    update input textfields

--- src/pages/TimeZoneConverter/index.vue
+++ src/pages/TimeZoneConverter/index.vue
@@ -45,14 +45,9 @@ const handleSelectChange = () => {
                     </strong>
                 </h4>
                 <div class="d-flex flex-column gap-2">
-                    <div class="mt-2"><strong>Select date and time </strong></div>
+                    <div class="mt-2 text-muted"><strong>Select date and time </strong></div>
                     <VueDatePicker v-model="date" />
-                    <div><strong> Select current timezone </strong></div>
-                    <select class="form-select" @change="handleSelectChange" name="timezone-select" id="timezone-select"
-                        v-model="timezone">
-                        <option :value="undefined">Select source timezone</option>
-                        <option v-for="zone in timezones" :value="zone" :key="zone">{{ zone }}</option>
-                    </select>
+                    <!-- <div><strong> Select current timezone </strong></div> -->
                     <div class="d-flex gap-1">
                         <input class="form-check-input" @input="handleCheckbox" v-model="localTimeCheck" type="checkbox"
                             id="flexCheckChecked">
@@ -60,13 +55,24 @@ const handleSelectChange = () => {
                             Choose my current timezone
                         </label>
                     </div>
-                    <div><strong>Destination timezone </strong></div>
-                    <div class="text-muted">Timezone to convert the source time into</div>
-                    <select class="form-select" name="timezone-select" id="timezone-select" v-model="reqTimezone">
-                        <option :value="undefined">Select destination timezone</option>
-                        <option v-for="zone in timezones" :value="zone" :key="zone">{{ zone }}</option>
-                    </select>
+                    <div class="form-floating">
+                        <select class="form-select" @change="handleSelectChange" name="timezone-select" id="timezone-select"
+                            v-model="timezone">
+                            <option :value="undefined">Choose an option</option>
+                            <option v-for="zone in timezones" :value="zone" :key="zone">{{ zone }}</option>
+                        </select>
+                        <label for="timezone-select">Select source timezone</label>
+                    </div>
 
+                    <!-- <div><strong>Destination timezone </strong></div> -->
+                    <!-- <div class="text-muted">Timezone to convert the source time into</div> -->
+                    <div class="form-floating">
+                        <select class="form-select" name="timezone-select" id="des-timezone-select" v-model="reqTimezone">
+                            <option :value="undefined">Select destination timezone</option>
+                            <option v-for="zone in timezones" :value="zone" :key="zone">{{ zone }}</option>
+                        </select>
+                        <label for="des-timezone-select">Destination timezone</label>
+                    </div>
                     <button :disabled="date === null || timezone === undefined || reqTimezone === undefined"
                         @click="convert" class="btn btn-primary">
                         Convert
