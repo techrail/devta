eac363e5992c337e01be2e954c346cfd1c69a1f0
Harikrishna-28 <harikrishnac2002@gmail.com>
2023-08-20 12:57:57 +0530

    refactor

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import Sidebar from './components/Sidebar/index.vue'
 </script>
 
 
 <template>
-    <main>
-        <RouterView />
+    <main class="d-flex">
+        <div class="w-100">
+            <RouterView />
+        </div>
     </main>
 </template>
