55be5174f4994c35bcc1a56caa3a9a62872b6703
Harikrishna-28 <harikrishnac2002@gmail.com>
2023-08-21 14:30:47 +0530

    refactor components

--- src/pages/UnixToDate/index.vue
+++ src/pages/UnixToDate/index.vue
@@ -5,6 +5,7 @@ import DateTimeConstants from '../../components/DateTimeConstants/DateTimeConsta
 import { copyToClipboard } from '../../components/utils/UnixDateTimeFunctions';
 import CopyContainer from '../../components/CopyContainer/CopyContainer.vue';
 import { covertCamelCase } from '../../components/utils/jsonBeautifier'
+import PageHeader from '../../components/Pageheader/PageHeader.vue';
 
 const unix = ref()
 const data = ref()
@@ -22,30 +23,29 @@ const handleClick = (value) => {
 </script>
 
 <template>
-    <div class="grid bg-light">
-        <div class="block card block1">
-            <div class="p-3">
-                <h4>
-                    <strong>
-                        Unix to Date converter
-                    </strong>
-                </h4>
-                <input v-model="unix" @input="handleChange" autofocus type="text" class="form-control mono-font"
-                    placeholder="Enter unix timestamp">
-
-                <div class="mt-2 p-2">
-                    <DateTimeConstants />
+    <main class="bg-light p-0 m-0 w-100">
+        <div class="w-100 mt-3">
+            <PageHeader />
+        </div>
+        <div class="grid bg-light">
+            <div class="block card block1">
+                <div class="p-3">
+                    <input v-model="unix" @input="handleChange" autofocus type="text" class="form-control mono-font"
+                        placeholder="Enter unix timestamp">
+                    <div class="mt-2 p-2">
+                        <DateTimeConstants />
+                    </div>
+                </div>
+            </div>
+            <div class="block card block2 overflow-auto">
+                <div v-if="unix" class="mt-3">
+                    <div v-for="(value, key) in data" class="p-1 d-flex flex-column gap-2" :key="key">
+                        <CopyContainer :title="key" :value="value" />
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="block card block2 overflow-auto">
-            <div v-if="unix" class="mt-3">
-                <div v-for="(value, key) in data" class="p-1 d-flex flex-column gap-2" :key="key">
-                    <CopyContainer :title="key" :value="value" />
-                </div>
-            </div>
-        </div>
-    </div>
+    </main>
 </template>
 
 <style scoped src="./style.css" />
