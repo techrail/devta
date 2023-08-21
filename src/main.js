f293ab27fa02ec6e0c0a1634e409087126075df1
Harikrishna-28 <harikrishnac2002@gmail.com>
2023-08-21 17:26:35 +0530

    remove balmui

--- src/main.js
+++ src/main.js
@@ -1,21 +1,21 @@
 import { createApp } from "vue";
 import "./style.css";
 import Index from "@pages/Index/index.vue";
-import Module from "@pages/Module/index.vue";
 import ErrorPage from "@pages/Error/index.vue";
-import BalmUI from "balm-ui";
 import { createPinia } from "pinia";
 import { createRouter, createWebHistory } from "vue-router";
+
+// styles
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap";
-import "balm-ui-css";
-import AppVue from "./App.vue";
-
 import hljs from "highlight.js/lib/core";
 import json from "highlight.js/lib/languages/json";
 import hljsVuePlugin from "@highlightjs/vue-plugin";
 import "highlight.js/styles/atom-one-light.css";
+
+// components
+import AppVue from "./App.vue";
 import HashGenerator from "./pages/HashGenerator/index.vue";
 import Base64Text from "./pages/Base64Text/index.vue";
 import DateToUnix from "./pages/DateToUnix/index.vue";
@@ -97,13 +97,6 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
-});
-
-app.use(BalmUI, {
-  $theme: {
-    primary: "#000000",
-    surface: "#000000",
-  },
 });
 
 app.use(router);
