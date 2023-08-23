8c779904c41be3064e9dc22c63fb24b77a90bb2d
Vaibhav Kaushal <vaibhavkaushal123@gmail.com>
2023-08-23 20:04:25 +0530

    added the message

--- src/pages/Index/index.vue
+++ src/pages/Index/index.vue
@@ -12,8 +12,11 @@ import TopBarButtons from "../../components/TopBarButtons/TopBarButtons.vue";
 
       <p class="p-1 text-muted lead text-center">
         Elevate your development experience with Devta, the ultimate tool for
-        simplifying and enhancing your daily tasks. Choose an option from the
-        menu start working.
+        simplifying and enhancing your daily tasks. <br/>
+        Choose an option from the menu to start working. <br/>
+        <strong>We do not send any data you submit to any server.</strong> <br/>
+        <strong><em><u>All data validation, processing and formatting is done in your browser.</u></em></strong><br/>
+        <em>You may check out the source code on our Github Repo!</em>
       </p>
       <div class="d-flex w-100 gap-2 justify-content-center align-items-center">
         <TopBarButtons />
