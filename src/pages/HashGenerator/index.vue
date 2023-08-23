98b234952f35c3cf71665ebb2793857eff7b59e8
Harikrishna-28 <harikrishnac2002@gmail.com>
2023-08-23 23:16:49 +0530

    update hash generator

--- src/pages/HashGenerator/index.vue
+++ src/pages/HashGenerator/index.vue
@@ -24,6 +24,8 @@ let timeout = undefined;
 function handleChange() {
     // console.log(value.value)
     // value.value = value;
+    outputs.value = Object.keys(algos).map(e => algos[e](value.value).toString());
+    return
     clearTimeout(timeout);
     timeout = setTimeout(() => {
         outputs.value = Object.keys(algos).map(e => algos[e](value.value).toString());
@@ -48,35 +50,26 @@ async function copyContent(idx) {
             <div class="block card block1">
                 <div class="w-100 p-3">
                     <div class="form-floating">
-                        <textarea v-model="value" @input="handleChange" autofocus type="text" class="form-control mono-font"
-                            id="textInput" style="height: 200px;" placeholder="Enter text">
+                        <textarea v-model="value" autofocus type="text" class="form-control mono-font" id="textInput"
+                            style="height: 200px;" placeholder="Enter text">
                     </textarea>
                         <label for="textInput">Enter text</label>
                     </div>
-                    <!-- <input @input="handleChange" v-model="value" autofocus type="text" class="form-control mono-font"
-                        placeholder="Enter unix timestamp"> -->
-                    <!-- <label for="input">Input:</label> -->
-
-                    <!-- <ui-textfield inputId="input" outlined input-type="textarea" v-model="value" fullwidth
-                        class="input fullWidth" placeholder="Enter text to hash..."
-                        @update:model-value="handleChange"></ui-textfield> -->
+                    <div class="row" style="padding-top: 16px;">
+                        <div class="w-50">
+                            <button class="btn btn-warning" @click="handleChange">Compute hash</button>
+                        </div>
+                    </div>
                 </div>
             </div>
 
             <div class="block card block2 overflow-auto">
                 <div v-for="(element, index) in Object.keys(algos)">
-                    <!-- <ui-textfield input-type="text" outlined disabled fullwidth class="input" v-model="outputs[index]">
-                    {{ element }}
-                    <template #after>
-                        <ui-textfield-icon @click="copyContent(index)" class="copy-icon">content_copy</ui-textfield-icon>
-                    </template>
-                </ui-textfield> -->
                     <div class="input-group p-1">
                         <span class="input-group-text">{{ element }}</span>
                         <input type="text" class="form-control" :value="outputs[index]" disabled readonly>
                         <span class="input-group-text" @click="copyContent(index)"><i class="bi bi-clipboard"></i></span>
                     </div>
-
                 </div>
             </div>
         </div>
