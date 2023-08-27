91ba6f2a8cd90e851de007f2a573066235153c7a
Harikrishna-28 <harikrishnac2002@gmail.com>
2023-08-27 13:09:25 +0530

    add localstorage to theme preference

--- src/stores/theme.js
+++ src/stores/theme.js
@@ -14,19 +14,39 @@ const darkThemePreference = () => {
   }
 };
 
+// checks for localstorage for theme preference
+// Defaults to system theme
+const setTheme = () => {
+  try {
+    const localStorageTheme = localStorage.getItem("dark-theme");
+    if (localStorageTheme === null) {
+      const defaultSystemTheme = localStorage.setItem(
+        "dark-theme",
+        darkThemePreference()
+      );
+      return defaultSystemTheme;
+    }
+    return JSON.parse(localStorageTheme);
+  } catch (error) {
+    // defaults to lightmode in case of error
+    return false;
+  }
+};
+
 // updates the theme
 export const updateTheme = (theme) => {
   try {
     document
       .querySelector("body")
       ?.setAttribute("data-bs-theme", theme ? "dark" : "light");
+    localStorage.setItem("dark-theme", JSON.stringify(theme));
   } catch (error) {
     console.log(error.message);
   }
 };
 
 export const useThemeStore = defineStore("theme", {
-  state: () => ({ darkTheme: darkThemePreference() }),
+  state: () => ({ darkTheme: setTheme() }),
   getters: {
     getTheme: (state) => state.theme,
   },
