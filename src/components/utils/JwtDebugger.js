74ba62a658d174928000491f7ddf0fdc22e03d06
Harikrishna-28 <harikrishnac2002@gmail.com>
2023-09-12 23:36:10 +0530

    add signature verification section

--- src/components/utils/JwtDebugger.js
+++ src/components/utils/JwtDebugger.js
@@ -1,7 +1,34 @@
-import { decodeProtectedHeader, decodeJwt } from "jose";
+import { decodeProtectedHeader, decodeJwt, jwtVerify } from "jose";
+
+export const algorithms = [
+  "HS256", // default arg
+  "HS384",
+  "HS512",
+  "PS256",
+  "PS384",
+  "PS512",
+  "RS256",
+  "RS384",
+  "RS512",
+  "ES256",
+  "ES384",
+  "ES512",
+];
 
 export const getHeader = (header, indent = 2) =>
   JSON.stringify(decodeProtectedHeader(header), null, indent);
 
 export const getPayload = (data, indent = 2) =>
   JSON.stringify(decodeJwt(data), null, indent);
+
+export const validateSignature = async (jwtToken, algorithm, key) => {
+  try {
+    if (algorithm[0].toLowercase() == "h") {
+      const key = new TextEncoder().encode(key);
+      const res = await jwtVerify(jwtToken, key);
+      console.log(res);
+    }
+  } catch (error) {
+    return false;
+  }
+};
--- src/pages/JWTDebugger/index.vue
+++ src/pages/JWTDebugger/index.vue
@@ -1,21 +1,33 @@
 <script setup>
-import { ref, watch, onMounted } from "vue";
+import { ref, watch, onMounted, watchEffect } from "vue";
 import { copyToClipboard } from "../../components/utils/UnixDateTime";
-import { getHeader } from "../../components/utils/JwtDebugger";
+import { algorithms, getHeader } from "../../components/utils/JwtDebugger";
 import { getPayload } from "../../components/utils/JwtDebugger";
 import PageHeader from "../../components/Pageheader/index.vue";
+import SignatureInput from "../../components/JWTSignatureVerify/SignatureInput.vue";
 
 onMounted(() => document.querySelector("[autofocus]")?.focus());
 
-const jwtoken = ref();
+const jwtoken = ref('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c');
 const decodedPayload = ref();
 const decodedHeader = ref();
+const validSig = ref(true)
+const selectedAlgorithm = ref(algorithms[0])
 
-watch(jwtoken, (newjwtoken, oldjwtoken) => {
-  if (!newjwtoken | (newjwtoken === oldjwtoken)) return;
-  decodedHeader.value = getHeader(newjwtoken);
-  decodedPayload.value = getPayload(newjwtoken);
-});
+// watch(jwtoken, (newjwtoken, oldjwtoken) => {
+//   if (!newjwtoken | (newjwtoken === oldjwtoken)) return;
+//   decodedHeader.value = getHeader(newjwtoken);
+//   decodedPayload.value = getPayload(newjwtoken);
+// });
+
+
+watchEffect(() => {
+  if (!jwtoken.value) return
+  decodedHeader.value = getHeader(jwtoken.value);
+  decodedPayload.value = getPayload(jwtoken.value);
+  const header = JSON.parse(decodedHeader.value)
+  selectedAlgorithm.value = header.alg
+})
 
 const handleClick = async (text) => {
   try {
@@ -24,6 +36,12 @@ const handleClick = async (text) => {
     console.log(error);
   }
 };
+
+const handleChange = (value) => {
+
+}
+
+
 </script>
 
 <template>
@@ -34,33 +52,56 @@ const handleClick = async (text) => {
     <div class="grid mt-1">
       <div class="block card block1">
         <div class="p-3">
+          <div class="alert alert-warning" role="alert">
+            <small>
+              Warning: JWTs are credentials, which can grant access to resources. Be careful where you paste them! We do
+              not record tokens, all
+              validation and debugging is done on the client side.
+            </small>
+          </div>
           <div class="overflow-auto">
             <div class="form-floating">
-              <textarea v-model="jwtoken" autofocus type="text" class="form-control mono-font" id="tokenInput" placeholder="Enter the token">
+              <textarea v-model="jwtoken" autofocus type="text" class="form-control mono-font" id="tokenInput"
+                placeholder="Enter the token">
               </textarea>
               <label for="tokenInput">Enter the token</label>
             </div>
           </div>
+
         </div>
       </div>
       <div class="block card block2 overflow-auto">
         <div class="p-2">
-          <div class="alert alert-warning" role="alert">
-            <small>
-              Warning: JWTs are credentials, which can grant access to resources. Be careful where you paste them! We do not record tokens, all
-              validation and debugging is done on the client side.
-            </small>
+
+          <!-- algorithm dropdown -->
+          <div class="form-floating mt-2">
+            <select class="form-select" name="algorithm-select" id="algorithm-select" v-model="selectedAlgorithm">
+              <option :value="algorithms[0]">{{ algorithms[0] }}</option>
+              <option v-for="algo in algorithms.slice(1)" :value="algo" :key="algo">{{ algo }}</option>
+            </select>
+            <label for="algorithm-select">Algorithm</label>
           </div>
         </div>
-        <div v-if="jwtoken">
+
+        <div v-if="jwtoken && decodedHeader && decodedPayload">
           <div class="p-2 overflow-auto">
-            <h5 class="text-muted">Header</h5>
-            <highlightjs :code="decodedHeader" />
+            <div class="">
+              <h5 class="text-muted"><strong>Header</strong></h5>
+              <highlightjs :code="decodedHeader" />
+            </div>
+            <div class="">
+              <h5 class="text-muted"><strong>Payload</strong></h5>
+              <highlightjs :code="decodedPayload" />
+            </div>
+            <!-- signature verification component -->
+            <div>
+              <h5 class="text-muted"><strong>Verify Signature</strong></h5>
+              <SignatureInput :algo-type="selectedAlgorithm" @key-change="(value) => handleChange(value)" />
+            </div>
+
           </div>
-          <div class="p-2 overflow-auto">
-            <h5 class="text-muted">Payload</h5>
-            <highlightjs :code="decodedPayload" />
-          </div>
+
+
         </div>
       </div>
     </div>
