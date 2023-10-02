8a6a89d14bd05300e7d7b06e4a63a9bec49f6ffb
Ansh <ansharorake2@gmail.com>
2023-10-02 15:12:44 +0530

    style: format code

--- src/pages/CRONParser/index.vue
+++ src/pages/CRONParser/index.vue
@@ -8,7 +8,13 @@
       <div class="block card block1">
         <div class="p-3">
           <div class="overflow-auto">
-            <input v-model="cronExpression" @input="parseCronExpression" type="text" class="form-control mono-font" placeholder="Enter CRON expression" />
+            <input
+              v-model="cronExpression"
+              @input="parseCronExpression"
+              type="text"
+              class="form-control mono-font"
+              placeholder="Enter CRON expression"
+            />
           </div>
         </div>
       </div>
@@ -23,43 +29,41 @@
         <div class="p-3">
           <h3>CRON Expression Syntax</h3>
           <table class="table table-bordered">
-  <thead>
-    <tr>
-      <th>Field</th>
-      <th>Description</th>
-      <th>Allowed Values</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>Minute</td>
-      <td>Minutes within the hour</td>
-      <td>0-59</td>
-    </tr>
-    <tr>
-      <td>Hour</td>
-      <td>Hours within the day</td>
-      <td>0-23</td>
-    </tr>
-    <tr>
-      <td>Day of Month</td>
-      <td>Day of the month</td>
-      <td>1-31</td>
-    </tr>
-    <tr>
-      <td>Month</td>
-      <td>Months of the year</td>
-      <td>1-12 or JAN-DEC</td>
-    </tr>
-    <tr>
-      <td>Day of Week</td>
-      <td>Days of the week</td>
-      <td>0-6 or SUN-SAT (0 and 7 represent Sunday)</td>
-    </tr>
-    <!-- You can add more rows for additional fields if needed -->
-  </tbody>
-</table>
-
+            <thead>
+              <tr>
+                <th>Field</th>
+                <th>Description</th>
+                <th>Allowed Values</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Minute</td>
+                <td>Minutes within the hour</td>
+                <td>0-59</td>
+              </tr>
+              <tr>
+                <td>Hour</td>
+                <td>Hours within the day</td>
+                <td>0-23</td>
+              </tr>
+              <tr>
+                <td>Day of Month</td>
+                <td>Day of the month</td>
+                <td>1-31</td>
+              </tr>
+              <tr>
+                <td>Month</td>
+                <td>Months of the year</td>
+                <td>1-12 or JAN-DEC</td>
+              </tr>
+              <tr>
+                <td>Day of Week</td>
+                <td>Days of the week</td>
+                <td>0-6 or SUN-SAT (0 and 7 represent Sunday)</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
