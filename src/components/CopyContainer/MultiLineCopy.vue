1433709d53c63c95093d4d2eb87b7176437536d9
Harikrishna-28 <harikrishnac2002@gmail.com>
2023-10-02 09:18:07 +0530

    refactor using ui components

--- src/components/CopyContainer/MultiLineCopy.vue
+++ src/components/CopyContainer/MultiLineCopy.vue
@@ -1,9 +1,12 @@
 <script setup>
+import { onMounted } from "vue"
+import { setTextInputSize } from "../utils/resizableInput"
 import { copyToClipboard } from "../utils/UnixDateTime"
 const props = defineProps({
     title: String,
     value: String | Number,
-    height: String
+    height: String,
+    key: Number
 })
 
 
@@ -11,16 +14,34 @@ const handleClick = () => {
     copyToClipboard(props.value)
 }
 
+// // generates a randomised id for the resizable form to work
+// const generateRandom = (length = 10) => {
+//     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+//     let result = '';
+//     for (let i = 0; i < length; i++) {
+//         const randomIndex = Math.floor(Math.random() * characters.length);
+//         result += characters.charAt(randomIndex);
+//     }
+//     return result;
+// }
+
+// onMounted(() => {
+//     const randomId = "resizable-input-" + generateRandom()
+//     const targetDiv = document.getElementById('resizable-form')
+//     targetDiv.id = randomId
+//     setTextInputSize([randomId])
+// })
+
 const style = {
     height: props.height
 }
 
+
 </script>
 
 <template>
-    <form class="form-floating d-flex gap-1 w-full">
-        <textarea type="text" rows="10" class="form-control" :style="style" readonly id="floatingInputValue"
-            :value="value" />
+    <form class="form-floating d-flex gap-1 w-full" id="resizable-form">
+        <textarea type="text" class="form-control" :style="style" readonly id="floatingInputValue" :value="value" />
         <label for="floatingInputValue">{{ title }}</label>
         <button @click="handleClick" class="btn btn-outline-secondary" type="button" id="copyButton">
             <i class="bi bi-clipboard"></i>
